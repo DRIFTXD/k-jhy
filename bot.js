a54746243d8bfa1e5c2314496bd329403ff218a2
DRIFTXD <42334507+DRIFTXD@users.noreply.github.com>
2019-07-14 16:08:45 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['ahelp I DRIFTXD - دريفت','Type ahelp'];
+    var setGame = ['ahelp I DRIFTXD - دريفت','http://lord-bot.ml/'];
     var i = -1;
     var j = 0;
     setInterval(function (){
